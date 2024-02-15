4bb8235aad1ea623b1a24eae464e5b560c2b04fd
GabyGarcas <gabybcr1542@gmail.com>
2024-02-15 17:33:21 +0100

    cambio en atendee

--- src/main/java/com/example/entities/Attendee.java
+++ src/main/java/com/example/entities/Attendee.java
@@ -14,6 +14,8 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -35,24 +37,27 @@ public class Attendee implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    //@NotNull(message = "The name of the attendee cannot be null")
-    //@NotEmpty(message = "The name cannot be empty")
-    
+  
+    @NotEmpty(message = "The name cannot be empty")
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "Write alphabetic letters only ")
     private String name;
 
-   // @NotNull(message = "The surname of the attendee cannot be null")
-    //@NotEmpty(message = "The surname cannot be empty")
-    // @NotNull(message = "The surname of the attendee cannot be null")
-    // @NotEmpty(message = "The surname cannot be empty")
+    
+    @NotEmpty(message = "The surname cannot be empty")
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "Write alphabetic letters only ")
     private String surname;
 
     //@Size(min = 5, max = 9, message = "the number of characters of this id cannot be less than 5 or more than 9")
-   // @Size(min = 5, max = 9, message = "the number of characters of this id cannot be less than 5 or more than 9")
     private int globalId;
 
 
-    // @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.REFRESH)
-    //private Event event;
+    @JoinTable(name = "event_seq",
+    joinColumns = { @JoinColumn(name = "attendee_id") },
+    inverseJoinColumns = { @JoinColumn(name = "event_id") })
+
+
+    private List <Event> events;
+    
 
     @ManyToMany(fetch = FetchType.LAZY,
     cascade = {
@@ -60,14 +65,12 @@ public class Attendee implements Serializable {
         CascadeType.MERGE
     })
 
-    @JoinTable(name = "event_seq",
-    joinColumns = { @JoinColumn(name = "attendee_id") },
-    inverseJoinColumns = { @JoinColumn(name = "event_id") })
 
-
-    private List<Event> events;
-
+    
+    @NotEmpty(message = "The field mail cannot be empty")
     private String mail;
+
+    @NotEmpty(message = "The profile cannot be empty")
     private Options options;
 
 
