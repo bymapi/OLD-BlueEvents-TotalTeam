c114aa901f637abeed053f47b420df1d372b108f
oumaymaBombarek <total.oumayma82@gmail.com>
2024-02-15 17:13:05 +0100

    Servicio del equipo Hecha

--- src/main/java/com/example/services/EventsServices.java
+++ src/main/java/com/example/services/EventsServices.java
@@ -1,5 +1,17 @@
 package com.example.services;
 
+import java.util.List;
+
+import com.example.entities.Event;
+
 public interface EventsServices {
+
+    public List<Event> findAll();
+
+    public Event findById(int id);
+
+    public void save(Event event);
+
+    public void delete(Event event);
 
 }
