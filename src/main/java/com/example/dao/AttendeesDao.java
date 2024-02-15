bd161d87e5c5310cf72b31191b99f332a69e2db9
oumaymaBombarek <total.oumayma82@gmail.com>
2024-02-15 16:30:28 +0100

    Otro

--- src/main/java/com/example/dao/AttendeesDao.java
+++ src/main/java/com/example/dao/AttendeesDao.java
@@ -2,18 +2,6 @@ package com.example.dao;
 
 public interface AttendeesDao {
 
-    /**
-     * Vamos a utilizar métodos personalizados,
    
-     * 
-     * 1. Recupera los asistentes paginados, es decir, de 10 en 10, de 20 en 20, etc
-     * 2. Recupera los asistentes ordenados, sin paginación
-     * 3. Dado el Global ID de un asistente recuperarle 
-     * 
-     * Para ello vamos a utilizar el lenguaje HQL (Hybernate Query Language), es muy similar a SQL pero
-     * lo que se consulta son las entidades, no las tablas.
-     * 
-     * Y no se pueden utilizar consultas de SQL nativas porque no soportan la paginacion y el ordenamiento
-     */ 
 
 }
