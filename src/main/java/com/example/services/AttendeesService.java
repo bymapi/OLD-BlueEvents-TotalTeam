4f81c4a807d044acc8fa1bf3a86026e975400321
bymapi <mapihernandezc@gmail.com>
2024-02-15 17:40:43 +0100

    AttendeesService cfeated, done

--- src/main/java/com/example/services/AttendeesService.java
+++ src/main/java/com/example/services/AttendeesService.java
@@ -1,5 +1,19 @@
 package com.example.services;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import com.example.entities.Attendee;
+
 public interface AttendeesService {
+    public Page<Attendee> findAll(Pageable pageable);
+    public List<Attendee> findAll(Sort sort);
+    public List<Attendee> findAll();
+    public Attendee findById(int id);
+    public Attendee save(Attendee attendees);
+    public void delete(Attendee attendees);
 
 }
